93a0a0415343843f9731d4917720e8e9df79b7f8
silence <asml.silence@gmail.com>
2015-08-09 09:20:13 +0300

    Sloppy select#type processing ref + code elimination

--- js/schedule.js
+++ js/schedule.js
@@ -4,7 +4,7 @@ $(function () {
 
     var $group = $("#group");
     var $grade = $("#course");
-    var $teacher =  $("#teacher");
+    var $teacher = $("#teacher");
     var $day = $("#day");
     var $room = $("#auditory");
     var $type = $('#type');
@@ -12,85 +12,91 @@ $(function () {
 
     createTimeTable();
     $('.week_now').text('Сейчас верхняя неделя'); // ToDo: Получение данных у backend'а
+    $(window).resize(function () {
+        optimizationTimeTable();
+    });
 
 
     // j - строка таблицы
     // i - столбец таблицы
+
+    var showList = {
+        group: [$grade, $group],
+        teacher: [$teacher],
+        auditory: [$room]
+    };
+
+
+    var typeVisualization = function(type) {
+        $selectList.forEach(function (elem) {
+            elem.hide();
+        });
+        if (type in showList) {
+            var someVar = showList[type];
+            someVar[0].removeAttr('disabled');
+
+            var i;
+            for (i = 1; i < someVar.length; ++i) {
+                someVar[i].attr("disabled", true).val('0');
+            }
+
+
+            showList[type].forEach(function (elem) {
+                elem.show();
+            });
+        }
+    };
+
+    var nameMap = {
+        group: 'grade',
+        course: 'grade',
+        auditory: 'room',
+        teacher: 'teacher'
+    };
+
+    var callbackMap = {
+        group: function (data) {
+            $grade.html('<option value="0">Выберите курс:</option>'); // Сначала чистим select
+            $.each(data, function (i, grade) {
+                $grade.append('<option value="' + grade.id + '">' + grade.degree + grade.num + '</option>');
+            });
+        },
+
+        teacher: function (data) {
+            $teacher.html('<option value="0">Выберите преподавателя:</option>'); // Сначала чистим select
+            $.each(data, function (i, teacher) {
+                $teacher.append('<option value="' + teacher.id + '">' + teacher.name + '</option>');
+            });
+        },
+
+        auditory: function (data) {
+            $room.html('<option value="0">Выберите аудиторию:</option>'); // Сначала чистим select
+            $.each(data, function (i, room) {
+                $room.append('<option value="' + room.id + '">' + room.title + '</option>');
+            });
+        }
+    };
+
+    var processProto = function(type, callback) {
+        var url = host + ':3000/' + nameMap[type] + '/list';
+        typeVisualization(type);
+        $.getJSON(url, callbackMap[type]);
+    };
+
+
+
 
 
 
 
 
     var invalidateType = function() {
-        $selectList.forEach(function(ind, elem) {
+        $selectList.forEach(function(elem) {
             elem.attr("disabled", "disabled").val('0');
         });
     };
 
-    var processTypeGroup = function() {
-        $grade.removeAttr("disabled");
-        $.getJSON(host + ':3000/grade/list', function (data) {
-            $grade.html('<option value="0">Выберите курс:</option>'); // Сначала чистим select
-            $.each(data, function (i, grade) {
-                $grade.append('<option value="' + grade.id + '">' + grade.degree + grade.num + '</option>');
-            });
-        });
-        $group.val('0');
-        $group.attr("disabled", "disabled"); // Убирает баг
-        $grade.show();
-        $group.show();
-        $teacher.hide();
-        $room.hide();
-        $day.hide();
-    };
 
-    var processTypeTeacher = function() {
-        $teacher.removeAttr("disabled");
-        $.getJSON(host + ':3000/teacher/list', function (data) {
-            $teacher.html('<option value="0">Выберите преподавателя:</option>'); // Сначала чистим select
-            $.each(data, function (i, teacher) {
-                $teacher.append('<option value="' + teacher.id + '">' + teacher.name + '</option>');
-            });
-        });
-        $grade.hide();
-        $group.hide();
-        $teacher.show();
-        $room.hide();
-        $day.hide();
-    };
-
-    var processTypeRoom = function() {
-        $room.removeAttr("disabled");
-        $.getJSON(host + ':3000/classrooms/list', function (data) {
-            $room.html('<option value="0">Выберите аудиторию:</option>'); // Сначала чистим select
-            $.each(data, function (i, room) {
-                $room.append('<option value="' + room.id + '">' + room.title + '</option>');
-            });
-        });
-        $grade.hide();
-        $group.hide();
-        $teacher.hide();
-        $room.show();
-        $day.hide();
-    };
-
-    var processTypeGrade = function() {
-        $grade.removeAttr("disabled");
-        console.log(host);
-        $.getJSON(host + ':3000/grade/list', function (data) {
-            $grade.html('<option value="0">Выберите курс:</option>'); // Сначала чистим select
-            $.each(data, function (i, grade) {
-                $grade.append('<option value="' + grade.id + '">' + grade.title + '</option>');
-            });
-        });
-        $group.val('0');
-        $group.attr("disabled", "disabled"); // Убирает баг
-        $grade.show();
-        $group.hide();
-        $teacher.hide();
-        $room.hide();
-        $day.show();
-    };
 
 
     // Выбор типа расписания
@@ -104,30 +110,17 @@ $(function () {
             invalidateType();
             return;
         }
-
-        switch (type) {
-            case 'group':
-                processTypeGroup();
-                break;
-            case 'teacher':
-                processTypeTeacher();
-                break;
-            case 'auditory':
-                processTypeRoom();
-                break;
-            case 'course':
-                processTypeGrade();
-                break;
-        }
+        processProto(type, callbackMap[type]);
     });
 
     // Группа -> Выбор курса
+    // Курс -> Выбор курса
     $grade.change(function () {
         if ($('#timetable_wrapper').css('display') == 'block') {
             hideTimeTable();
         }
 
-        if (!$grade.val() || $type.val() !== 'group') {
+        if (!$grade.val()) {
             $group.attr("disabled", "disabled");
             $group.val('0');
             return;
@@ -143,29 +136,29 @@ $(function () {
     });
 
     // Группа -> Курс -> Выбор группы [Вывод расписания]
-    $group.change(function () {
-        var group = $group.val();
-        if (!group) {
-            hideTimeTable();
-            return;
-        }
-
-        // todo: kill getdata.php
-        $.getJSON('getdata.php', {
-            'data': 'schedule',
-            'type': 'Group',
-            'course': $grade.val(),
-            'group': group
-        }, function (json_schedule) {
-            removeCells(); // Удаляем ячейки
-            createTimeTable();
-            outputSchedule(json_schedule.schedule, json_schedule.current_week);
-        });
-        $('.type_timetable').html(
-            $grade.children('option:selected').text() + ', ' + $group.children('option:selected').text()
-        );
-
-    });
+    //$group.change(function () {
+    //    var group = $group.val();
+    //    if (!group) {
+    //        hideTimeTable();
+    //        return;
+    //    }
+    //
+    //    // todo: kill getdata.php
+    //    $.getJSON('getdata.php', {
+    //        'data': 'schedule',
+    //        'type': 'Group',
+    //        'course': $grade.val(),
+    //        'group': group
+    //    }, function (json_schedule) {
+    //        removeCells(); // Удаляем ячейки
+    //        createTimeTable();
+    //        outputSchedule(json_schedule.schedule, json_schedule.current_week);
+    //    });
+    //    $('.type_timetable').html(
+    //        $grade.children('option:selected').text() + ', ' + $group.children('option:selected').text()
+    //    );
+    //
+    //});
 
 
     // Преподаватель -> Выбор преподавателя [Вывод расписания]
@@ -184,69 +177,66 @@ $(function () {
         $('.type_timetable').html($teacher.children('option:selected').text());
     });
 
-    // Аудитория -> Выбор аудитории [Вывод расписания]
-    $room.change(function () {
-        var room = $room.val();
-        if (!room) {
-            hideTimeTable();
-            return;
-        }
+    //// Аудитория -> Выбор аудитории [Вывод расписания]
+    //$room.change(function () {
+    //    var room = $room.val();
+    //    if (!room) {
+    //        hideTimeTable();
+    //        return;
+    //    }
+    //
+    //    // todo: kill getdata.php
+    //    $.getJSON('getdata.php', {
+    //        'data': 'schedule',
+    //        'type': 'Classroom',
+    //        'id': room
+    //    }, function (json_schedule) {
+    //        removeCells(); // Удаляем ячейки
+    //        createTimeTable();
+    //        outputSchedule(json_schedule.schedule, json_schedule.current_week);
+    //    });
+    //    $('.type_timetable').html('Аудитория ' + $room.children('option:selected').text());
+    //});
 
-        // todo: kill getdata.php
-        $.getJSON('getdata.php', {
-            'data': 'schedule',
-            'type': 'Classroom',
-            'id': room
-        }, function (json_schedule) {
-            removeCells(); // Удаляем ячейки
-            createTimeTable();
-            outputSchedule(json_schedule.schedule, json_schedule.current_week);
-        });
-        $('.type_timetable').html('Аудитория ' + $room.children('option:selected').text());
-    });
 
-
-    // Курс -> Выбор курса
-    $grade.change(function () {
-
-        if ($('#timetable_wrapper').css('display') == 'block') {
-            hideTimeTable();
-        }
-
-        if ($grade.val() && $type.val() === 'course') {
-            $day.removeAttr("disabled");
-        } else {
-            $day.attr("disabled", "disabled");
-        }
-        $day.val('0');
-    });
+    //// Курс -> Выбор курса
+    //$grade.change(function () {
+    //    if ($('#timetable_wrapper').css('display') == 'block') {
+    //        hideTimeTable();
+    //    }
+    //
+    //    if ($grade.val() && $type.val() === 'course') {
+    //        $day.removeAttr("disabled");
+    //    } else {
+    //        $day.attr("disabled", "disabled");
+    //    }
+    //    $day.val('0');
+    //});
 
 
     // Курс -> Выбор курса -> Выбор дня [Вывод расписания]
-    $day.change(function () {
-        var day = $day.val();
-        if (!$day.val()) {
-            hideTimeTable();
-            return;
-        }
+    //$day.change(function () {
+    //    var day = $day.val();
+    //    if (!$day.val()) {
+    //        hideTimeTable();
+    //        return;
+    //    }
+    //
+    //    // todo: kill getdata.php
+    //    $.getJSON('getdata.php', {
+    //        'data': 'schedule',
+    //        'type': 'Course',
+    //        'course': $grade.val(),
+    //        'day': day
+    //    }, function (data) {
+    //        removeCells(); // Удаляем ячейки
+    //        createTimeTableCourse2(data.schedule);
+    //        outputSchedule(data.schedule, data.current_week);
+    //    });
+    //    $('.type_timetable').html('Курс ' + $grade.children('option:selected').text());
+    //});
 
-        // todo: kill getdata.php
-        $.getJSON('getdata.php', {
-            'data': 'schedule',
-            'type': 'Course',
-            'course': $grade.val(),
-            'day': day
-        }, function (data) {
-            removeCells(); // Удаляем ячейки
-            createTimeTableCourse2(data.schedule);
-            outputSchedule(data.schedule, data.current_week);
-        });
-        $('.type_timetable').html('Курс ' + $grade.children('option:selected').text());
-    });
 
-    $(window).resize(function () {
-        optimizationTimeTable();
-    });
 
 
 
