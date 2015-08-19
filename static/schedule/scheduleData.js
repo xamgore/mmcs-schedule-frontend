10f1e2a5dbf2a73a7af12bb56831ab2988d425d9
silence <asml.silence@gmail.com>
2015-08-19 16:11:44 +0300

    Add staff data loaders

--- static/schedule/scheduleData.js
+++ static/schedule/scheduleData.js
@@ -18,12 +18,29 @@ $(function () {
     };
 
 
+    // =========================
+    //  menu data loaders
+    // =========================
+
     /**
      * @typedef {object} grade_t
      * @property {number} id
      * @property {number} num
      * @property {string} degree
      */
+    /**
+     * @callback gradeCallback
+     * @param {group_t[]} currentWeek
+     */
+    /**
+     * @param {gradeCallback} cb
+     */
+    loader.grades = function (cb) {
+        menu.getJSON('grade/list', function (data) {
+            cb(data);
+        });
+    };
+
 
     /**
      * @typedef {object} group_t
@@ -32,6 +49,129 @@ $(function () {
      * @property {number} gradeid
      * @property {string} [name]
      */
+    /**
+     * @callback groupCallback
+     * @param {grade_t[]} gradeList
+     */
+    /**
+     * @param {number} gradeID
+     * @param {groupCallback} cb
+     */
+    loader.groups = function (gradeID, cb) {
+        menu.getJSON('group/list/' + gradeID, function (data) {
+            cb(data);
+        });
+    };
+
+
+    /**
+     * @typedef {object} subject_t
+     * @property {number} id
+     * @property {string} name
+     * @property {string} abbr
+     */
+    /**
+     * @callback subjectCallback
+     * @param {subject_t[]} subjectList
+     */
+    /**
+     * @param {subjectCallback} cb
+     */
+    loader.subjects = function (cb) {
+        menu.getJSON('subject/list', function (data) {
+            cb(data);
+        });
+    };
+
+
+    /**
+     * @typedef {object} room_t
+     * @property {number} id
+     * @property {string} name
+     */
+    /**
+     * @callback roomCallback
+     * @param {room_t[]} roomList
+     */
+    /**
+     * @param {roomCallback} cb
+     */
+    loader.rooms = function (cb) {
+        menu.getJSON('room/list', function (data) {
+            cb(data);
+        });
+    };
+
+
+
+    /**
+     * @typedef {object} teacher_t
+     * @property {number} id
+     * @property {string} name
+     * @property {string} degree
+     */
+    /**
+     * @callback teacherCallback
+     * @param {teacher_t[]} teacherList
+     */
+    /**
+     * @param {teacherCallback} cb
+     */
+    loader.teachers = function (cb) {
+        menu.getJSON('teacher/list', function (data) {
+            cb(data);
+        });
+    };
+
+
+    /**
+     * @typedef {object} time_t
+     * @property {number} [hours]
+     * @property {number} [minutes]
+     */
+    /**
+     * @typedef {object} lessonTime_t
+     * @property {number} id
+     * @property {number} num
+     * @property {time_t} cbeg
+     * @property {time_t} cend
+     */
+    /**
+     * @callback timeCallback
+     * @param {lessonTime_t[]} lessonTimes
+     */
+    /**
+     * @param {timeCallback} cb
+     */
+    loader.times = function (cb) {
+        menu.getJSON('time/list', function (data) {
+            cb(data);
+        });
+    };
+
+
+    /**
+     * @callback weekCallback
+     * @param {number} groupList
+     */
+    /**
+     * @param {weekCallback} cb
+     * @param {jQuery} $week
+     */
+    loader.week = function ($week, cb) {
+        menu.getJSON('schedule/week', function (data) {
+            var actualWeek = data.type ? 'верхняя неделя' : 'нижняя неделя'; // todo: extract
+            $week.text('Сейчас ' + actualWeek);
+
+            if (cb) {
+                cb(data.type);
+            }
+        });
+    };
+
+
+
+
 
     // =========================
     //      name generators
@@ -79,58 +219,6 @@ $(function () {
      */
     menu.groupOption = function (group) {
         return '<option value="' + group.id + '">' + menu.groupName(group) + '</option>';
-    };
-
-
-    // =========================
-    //  menu data loaders
-    // =========================
-
-    /**
-     * @callback gradeCallback
-     * @param {group_t[]} currentWeek
-     */
-    /**
-     * @param {gradeCallback} cb
-     */
-    loader.grades = function (cb) {
-        menu.getJSON('grade/list', function (data) {
-            cb(data);
-        });
-    };
-
-    /**
-     * @callback groupCallback
-     * @param {grade_t[]} gradeList
-     */
-    /**
-     * @param {number} gradeID
-     * @param {groupCallback} cb
-     */
-    loader.groups = function (gradeID, cb) {
-        menu.getJSON('group/list/' + gradeID, function (data) {
-            cb(data);
-        });
-    };
-
-
-    /**
-     * @callback weekCallback
-     * @param {number} groupList
-     */
-    /**
-     * @param {weekCallback} cb
-     * @param {jQuery} $week
-     */
-    loader.week = function ($week, cb) {
-        menu.getJSON('schedule/week', function (data) {
-            var actualWeek = data.type ? 'верхняя неделя' : 'нижняя неделя'; // todo: extract
-            $week.text('Сейчас ' + actualWeek);
-
-            if (cb) {
-                cb(data.type);
-            }
-        });
     };
 
 
