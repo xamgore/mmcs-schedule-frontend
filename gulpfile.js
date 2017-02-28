2a9bfb308a25650cca32cf65c545b2366209bbfa
Владислав Яковлев <vlad309523@gmail.com>
2017-03-01 01:28:30 +0300

    Fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -62,22 +62,25 @@ let opts = {
 
 let config = require('./gulp-config');
 
-config.source.types.etc = { src: [ '**/*' ] };
+config.types.etc = { src: [ '**/*' ] };
 
-Object.keys(config.source.types).forEach(typeName => {
-    let type = config.source.types[typeName];
-    type.src = type.src.map(src => path.resolve(config.source.src, src));
-    type.dest = path.resolve(config.source.dest, type.dest || '');
+Object.keys(config.types).forEach(typeName => {
+    let type = config.types[typeName];
+    type.src = type.src.map(src => path.resolve(config.src, src));
+    type.dest = path.resolve(config.dest, type.dest || '');
     type.pipe = createPipe(type.pipe);
     type.minify = createPipe(type.minify);
 });
 
-Object.keys(config.source.types).forEach(typeName => {
+Object.keys(config.types).forEach(typeName => {
     if (typeName === 'etc') return;
 
-    let type = config.source.types[typeName];
-    type.src.forEach(src => config.source.types.etc.src.push(`!${src}`));
+    let type = config.types[typeName];
+    type.src.forEach(src => config.types.etc.src.push(`!${src}`));
 });
+
+config.cleanList = [ `${config.dest}/**`, `!${config.dest}` ];
+if (config.cleanIgnore) config.cleanIgnore.forEach(data => config.cleanList.push(`!${path.resolve(config.dest, data)}`));
 
 
 //===================================
@@ -85,18 +88,18 @@ Object.keys(config.source.types).forEach(typeName => {
 //===================================
 
 // Копирование файлов
-Object.keys(config.source.types).forEach(typeName => {
-    let type = config.source.types[typeName];
+Object.keys(config.types).forEach(typeName => {
+    let type = config.types[typeName];
     gulp.task(`source_${typeName}`, () => executeGulpTask(type));
 });
 
 gulp.task('source_copy', callback => runSequence(
-    Object.keys(config.source.types).map(typeName => `source_${typeName}`),
+    Object.keys(config.types).map(typeName => `source_${typeName}`),
     callback
 ));
 
 // Очистка папки сборки
-gulp.task('source_clean', callback => del([ `${config.source.dest}/**`, `!${config.source.dest}` ].concat(config.source.ignoreDel.map(data => `!${path.resolve(config.source.dest, data)}`)), callback));
+gulp.task('source_clean', callback => del(config.cleanList, callback));
 
 // Генерация source
 gulp.task('source', callback => runSequence(
@@ -107,8 +110,8 @@ gulp.task('source', callback => runSequence(
 
 // Отслеживание изменений
 gulp.task('source_watch', callback => {
-    Object.keys(config.source.types).forEach(typeName => {
-        let type = config.source.types[typeName];
+    Object.keys(config.types).forEach(typeName => {
+        let type = config.types[typeName];
         watch(type.src, vinyl => {
             switch (vinyl.event) {
                 case 'add':
@@ -117,20 +120,12 @@ gulp.task('source_watch', callback => {
                     break;
 
                 case 'unlink':
-                    console.log(`Уou must restart gulp to delete file "${path.relative(config.source.src, vinyl.path)}"`);
+                    console.log(`Уou must restart gulp to delete file "${path.relative(config.src, vinyl.path)}"`);
                     break;
             }
         });
     });
-    //callback();
 });
-
-
-//===================================
-//  Задачи по копированию config
-//===================================
-
-gulp.task('config', () => gulp.src(config.config.src).pipe(gulp.dest(config.config.dest)));
 
 
 //===================================
@@ -139,14 +134,12 @@ gulp.task('config', () => gulp.src(config.config.src).pipe(gulp.dest(config.conf
 
 // Задача по-умолчанию
 gulp.task('default', callback => runSequence(
-    'config',
     'source',
     callback
 ));
 
 // Отслеживание изменений
 gulp.task('watch', callback => runSequence(
-    'config',
     'source',
     'source_watch',
     callback
